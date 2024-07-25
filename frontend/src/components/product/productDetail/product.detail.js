4d438bd1a310b34cd0553d43654332f07174a424
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2024-07-25 19:58:59 -0400

    product detail page

--- frontend/src/components/product/productDetail/product.detail.js
+++ frontend/src/components/product/productDetail/product.detail.js
@@ -7,6 +7,7 @@ import { selectIsLoggedIn } from '../../../redux/features/auth/auth.slice'
 import { getProduct } from '../../../redux/features/product/product.slice'
 import Card from '../../card/card'
 import { SpinnerImage } from '../../loader/loader'
+import DOMPurify from "dompurify"
 
 const ProductDetail = () => {
     useRedirectLoggedOutUser("/login")
@@ -27,6 +28,13 @@ const ProductDetail = () => {
         }
     }, [isLoggedIn, isError, message, dispatch])
 
+    const stockStatus = (quantity) => {
+        if (quantity > 0) {
+            return <span className="--color-success">In Stock</span>
+        }
+        return <span className="--color-danger">Out of Stock</span>
+    }
+
     return (
         <div className="product-detail">
             <h3 className="--mt">Product Details</h3>
@@ -41,6 +49,32 @@ const ProductDetail = () => {
                                 <p>No image set for this product</p>
                             )}
                         </Card>
+                        <h4>Product Availability: {stockStatus(product.quantity)}</h4>
+                        <hr />
+                        <h4>
+                            <span className="badge">Name: </span> & nbsp {product.name}
+                        </h4>
+                            <p>
+                                <b>&rarr SKU :</b> {product.sku}
+                            </p>
+                            <p>
+                                <b>&rarr Category :</b> {product.category}
+                            </p>
+                            <p>
+                                <b>&rarr Price :</b> {product.price}
+                            </p>
+                            <p>
+                                <b>&rarr Quantity in Stock :</b> {"$"}{product.quantity}
+                            </p>
+                            <p>
+                                <b>&rarr Total Value in Stock :</b> {"$"}{product.price * product.sku}
+                            </p>
+                            <hr />
+                            <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(product.description)}}></div>
+                            <hr />
+                            <code className="--color-dark">Created on: {product.createdAt.toLocaleString("en-US")} </code>
+                            <br/>
+                            <code className="--color-dark">Last updated: {product.updatedAt.toLocaleString("en-US")} </code>
                     </div>
                 )}
             </Card>
