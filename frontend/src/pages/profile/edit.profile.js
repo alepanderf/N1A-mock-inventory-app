71b99e1a261ed20065ed0d0bff43063e4cbabe11
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2024-07-26 11:27:23 -0400

    fixing indentation error

--- frontend/src/pages/profile/edit.profile.js
+++ frontend/src/pages/profile/edit.profile.js
@@ -43,33 +43,33 @@ const EditProfile = () => {
                         <img src={user?.photo} alt="profilepic" />
                     </span>
                     <form className="--form-control" onSubmit={saveProfile}>
-                    <span className="profile-data">
-                        <p>
-                            <label>Name: </label>
-                            <input type="text" name="name" value={profile?.name} onChange={handleInputChange}/>
-                        </p>
-                        <p>
-                            <label>Email: </label>
-                            <input type="text" name="email" value={profile?.email} disabled/>
-                            <br/>
-                            <code>Email cannot be changed.</code>
-                        </p>
-                        <p>
-                            <label>Phone: </label>
-                            <input type="text" name="phone" value={profile?.phone} onChange={handleInputChange}/>
-                        </p>
-                        <p>
-                            <label>Bio: </label>
-                            <textarea name="bio" value={profile?.bio} onChange={handleInputChange} cols="30" rows="10"></textarea>
-                        </p>
-                        <p>
-                            <label>Photo: </label>
-                            <input type="file" name="image" onChange={handleImageChange}/>
-                        </p>
-                        <div>
-                            <button className="--btn --btn-primary">Edit Profile</button>
-                        </div>
-                    </span>
+                        <span className="profile-data">
+                            <p>
+                                <label>Name: </label>
+                                <input type="text" name="name" value={profile?.name} onChange={handleInputChange}/>
+                            </p>
+                            <p>
+                                <label>Email: </label>
+                                <input type="text" name="email" value={profile?.email} disabled/>
+                                <br/>
+                                <code>Email cannot be changed.</code>
+                            </p>
+                            <p>
+                                <label>Phone: </label>
+                                <input type="text" name="phone" value={profile?.phone} onChange={handleInputChange}/>
+                            </p>
+                            <p>
+                                <label>Bio: </label>
+                                <textarea name="bio" value={profile?.bio} onChange={handleInputChange} cols="30" rows="10"></textarea>
+                            </p>
+                            <p>
+                                <label>Photo: </label>
+                                <input type="file" name="image" onChange={handleImageChange}/>
+                            </p>
+                            <div>
+                                <button className="--btn --btn-primary">Edit Profile</button>
+                            </div>
+                        </span>
                     </form>
                 </Card>
         </div>
