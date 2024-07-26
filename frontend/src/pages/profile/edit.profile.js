877ab24fdc1149f1b2ef2bc45dba05d66e8076f5
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2024-07-26 15:37:17 -0400

    added useeffecct import statement

--- frontend/src/pages/profile/edit.profile.js
+++ frontend/src/pages/profile/edit.profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './profile.scss'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../../redux/features/auth/auth.slice'
