aa9c5460fc7e0b4db5a2799c179c387815167cd5
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2024-07-26 09:47:04 -0400

    built jsx for photo card in profile page

--- frontend/src/pages/profile/profile.js
+++ frontend/src/pages/profile/profile.js
@@ -4,6 +4,8 @@ import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser'
 import { useDispatch } from 'react-redux'
 import { getUser } from '../../services/auth.service'
 import { SET_NAME, SET_USER } from '../../redux/features/auth/auth.slice'
+import { SpinnerImage } from '../../components/loader/loader'
+import Card from '../../components/card/card'
 
 const Profile = () => {
     useRedirectLoggedOutUser("/login")
@@ -27,7 +29,18 @@ const Profile = () => {
     }, [dispatch])
     
     return (
-        <div>Profile</div>
+        <div className="profile --my2">
+            {isLoading && <SpinnerImage/>}
+            <>
+                {!isLoading && profile === null ? 
+                ( <p>Something went wrong, please reload page...</p>) : 
+                (<Card cardClass={"card --flex-dir-column"}>
+                    <span className="profile-photo">
+                        <img src={profile?.photo} alt="profilepic" />
+                    </span>
+                </Card>)}
+            </>
+        </div>
     )
 }
 
