fa8293b771802a8136c718cef6aa7a1fc2fadc8c
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2024-07-26 09:54:12 -0400

    built out jsx componenents for profile data

--- frontend/src/pages/profile/profile.js
+++ frontend/src/pages/profile/profile.js
@@ -6,6 +6,7 @@ import { getUser } from '../../services/auth.service'
 import { SET_NAME, SET_USER } from '../../redux/features/auth/auth.slice'
 import { SpinnerImage } from '../../components/loader/loader'
 import Card from '../../components/card/card'
+import { Link } from 'react-router-dom'
 
 const Profile = () => {
     useRedirectLoggedOutUser("/login")
@@ -37,6 +38,25 @@ const Profile = () => {
                 (<Card cardClass={"card --flex-dir-column"}>
                     <span className="profile-photo">
                         <img src={profile?.photo} alt="profilepic" />
+                    </span>
+                    <span className="profile-data">
+                        <p>
+                            <b>Name : </b> {profile?.name}
+                        </p>
+                        <p>
+                            <b>Email : </b> {profile?.email}
+                        </p>
+                        <p>
+                            <b>Phone : </b> {profile?.phone}
+                        </p>
+                        <p>
+                            <b>Bio : </b> {profile?.bio}
+                        </p>
+                        <div>
+                            <Link to="/edit-profile">
+                                <button className="--btn --btn-primary">Edit Profile</button>
+                            </Link>
+                        </div>
                     </span>
                 </Card>)}
             </>
